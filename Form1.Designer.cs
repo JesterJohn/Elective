6e5fa5c1ce0ef3bfdd420669fc3bc4e244564303
JesterJohn <jesterjohnmacatangay21@gmail.com>
2026-02-12 07:34:10 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace Elective
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.ProductName_Listbox = new System.Windows.Forms.ListBox();
+            this.Price_Listbox = new System.Windows.Forms.ListBox();
+            this.Qty_Listbox = new System.Windows.Forms.ListBox();
+            this.TotalAmt_Listbox = new System.Windows.Forms.ListBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.cashbtn = new System.Windows.Forms.Button();
+            this.checkbtn = new System.Windows.Forms.Button();
+            this.smartpaybtn = new System.Windows.Forms.Button();
+            this.cardbtn = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.exitbtn = new System.Windows.Forms.Button();
+            this.clearbtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(44, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Amount to Pay:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(278, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(202, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Cash Rendered:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(515, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(158, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Grand Total:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(49, 96);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(217, 35);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(283, 96);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(217, 35);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(520, 96);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(217, 35);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(771, 56);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(152, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Sale Summary: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(772, 106);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(114, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Product Name:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(924, 106);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 20);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Price:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(1023, 106);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(72, 20);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Quantity:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(1126, 106);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(108, 20);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Total Amount:";
+            // 
+            // ProductName_Listbox
+            // 
+            this.ProductName_Listbox.FormattingEnabled = true;
+            this.ProductName_Listbox.ItemHeight = 20;
+            this.ProductName_Listbox.Location = new System.Drawing.Point(776, 139);
+            this.ProductName_Listbox.Name = "ProductName_Listbox";
+            this.ProductName_Listbox.Size = new System.Drawing.Size(110, 464);
+            this.ProductName_Listbox.TabIndex = 12;
+            // 
+            // Price_Listbox
+            // 
+            this.Price_Listbox.FormattingEnabled = true;
+            this.Price_Listbox.ItemHeight = 20;
+            this.Price_Listbox.Location = new System.Drawing.Point(892, 139);
+            this.Price_Listbox.Name = "Price_Listbox";
+            this.Price_Listbox.Size = new System.Drawing.Size(110, 464);
+            this.Price_Listbox.TabIndex = 13;
+            // 
+            // Qty_Listbox
+            // 
+            this.Qty_Listbox.FormattingEnabled = true;
+            this.Qty_Listbox.ItemHeight = 20;
+            this.Qty_Listbox.Location = new System.Drawing.Point(1008, 139);
+            this.Qty_Listbox.Name = "Qty_Listbox";
+            this.Qty_Listbox.Size = new System.Drawing.Size(110, 464);
+            this.Qty_Listbox.TabIndex = 14;
+            // 
+            // TotalAmt_Listbox
+            // 
+            this.TotalAmt_Listbox.FormattingEnabled = true;
+            this.TotalAmt_Listbox.ItemHeight = 20;
+            this.TotalAmt_Listbox.Location = new System.Drawing.Point(1124, 139);
+            this.TotalAmt_Listbox.Name = "TotalAmt_Listbox";
+            this.TotalAmt_Listbox.Size = new System.Drawing.Size(110, 464);
+            this.TotalAmt_Listbox.TabIndex = 15;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(44, 174);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(176, 25);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Mode of Payment: ";
+            // 
+            // cashbtn
+            // 
+            this.cashbtn.Location = new System.Drawing.Point(49, 217);
+            this.cashbtn.Name = "cashbtn";
+            this.cashbtn.Size = new System.Drawing.Size(340, 39);
+            this.cashbtn.TabIndex = 17;
+            this.cashbtn.Text = "CASH";
+            this.cashbtn.UseVisualStyleBackColor = true;
+            // 
+            // checkbtn
+            // 
+            this.checkbtn.Location = new System.Drawing.Point(395, 217);
+            this.checkbtn.Name = "checkbtn";
+            this.checkbtn.Size = new System.Drawing.Size(342, 39);
+            this.checkbtn.TabIndex = 18;
+            this.checkbtn.Text = "CHECK";
+            this.checkbtn.UseVisualStyleBackColor = true;
+            // 
+            // smartpaybtn
+            // 
+            this.smartpaybtn.Location = new System.Drawing.Point(395, 273);
+            this.smartpaybtn.Name = "smartpaybtn";
+            this.smartpaybtn.Size = new System.Drawing.Size(342, 39);
+            this.smartpaybtn.TabIndex = 20;
+            this.smartpaybtn.Text = "SMARTPAY";
+            this.smartpaybtn.UseVisualStyleBackColor = true;
+            // 
+            // cardbtn
+            // 
+            this.cardbtn.Location = new System.Drawing.Point(49, 273);
+            this.cardbtn.Name = "cardbtn";
+            this.cardbtn.Size = new System.Drawing.Size(340, 39);
+            this.cardbtn.TabIndex = 19;
+            this.cardbtn.Text = "CARD";
+            this.cardbtn.UseVisualStyleBackColor = true;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(44, 349);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(184, 25);
+            this.label10.TabIndex = 21;
+            this.label10.Text = "Payment Summary:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(49, 389);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(687, 164);
+            this.listBox1.TabIndex = 22;
+            // 
+            // exitbtn
+            // 
+            this.exitbtn.Location = new System.Drawing.Point(394, 564);
+            this.exitbtn.Name = "exitbtn";
+            this.exitbtn.Size = new System.Drawing.Size(342, 39);
+            this.exitbtn.TabIndex = 24;
+            this.exitbtn.Text = "EXIT";
+            this.exitbtn.UseVisualStyleBackColor = true;
+            // 
+            // clearbtn
+            // 
+            this.clearbtn.Location = new System.Drawing.Point(48, 564);
+            this.clearbtn.Name = "clearbtn";
+            this.clearbtn.Size = new System.Drawing.Size(340, 39);
+            this.clearbtn.TabIndex = 23;
+            this.clearbtn.Text = "CLEAR";
+            this.clearbtn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(1253, 628);
+            this.Controls.Add(this.exitbtn);
+            this.Controls.Add(this.clearbtn);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.smartpaybtn);
+            this.Controls.Add(this.cardbtn);
+            this.Controls.Add(this.checkbtn);
+            this.Controls.Add(this.cashbtn);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.TotalAmt_Listbox);
+            this.Controls.Add(this.Qty_Listbox);
+            this.Controls.Add(this.Price_Listbox);
+            this.Controls.Add(this.ProductName_Listbox);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ListBox ProductName_Listbox;
+        private System.Windows.Forms.ListBox Price_Listbox;
+        private System.Windows.Forms.ListBox Qty_Listbox;
+        private System.Windows.Forms.ListBox TotalAmt_Listbox;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button cashbtn;
+        private System.Windows.Forms.Button checkbtn;
+        private System.Windows.Forms.Button smartpaybtn;
+        private System.Windows.Forms.Button cardbtn;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button exitbtn;
+        private System.Windows.Forms.Button clearbtn;
+    }
+}
+
